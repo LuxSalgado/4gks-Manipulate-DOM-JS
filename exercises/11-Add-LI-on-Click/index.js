e00f65ad36ff99bbe3c1ad41e25ee612f46cf204
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-31 23:51:20 +0000

    Ejercicios resueltos hasta el número 11

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	let myLi = document.createElement("LI");
+	myLi.innerHTML = "Fourth element";
+	let padre = document.querySelector("#myList");
+	padre.appendChild(myLi);
 });
