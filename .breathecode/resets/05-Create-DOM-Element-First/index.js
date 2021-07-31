37f6165b556fb66282d9e9353d38b6b7a2ab6c77
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-31 22:51:55 +0000

    Ejercicios resueltos hasta el número 6

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,8 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-
-let nuevoDiv = document.createElement("p");
-nuevoDiv.innerHTML = "Hello World";
-nuevoDiv.style.background = "yellow";
-document.body.appendChild(nuevoDiv);
