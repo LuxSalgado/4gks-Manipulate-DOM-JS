cdddbf3409698426be49094ae4e51fce089acaa1
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-07-31 23:37:55 +0000

    Ejercicios resueltos hasta el número 10

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
-
+	let miNuevoDiv = document.createElement("div");
+	miNuevoDiv.style.background = "yellow";
+	miNuevoDiv.innerHTML = "Hello World";
+	document.body.appendChild(miNuevoDiv);
 });
