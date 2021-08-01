1a15be00aaaea74a5c141777286b03c5c7130c69
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-01 00:40:50 +0000

    Ejercicios resueltos hasta el número 13

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,6 +1,24 @@
 window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
-
-  // your code here
+	// your code here
+	let padre = document.querySelector("#mySelect");
+	for (let i = 0; i < countries.length; i++) {
+		var opt = document.createElement("option");
+		opt.value = i;
+		opt.innerHTML = countries[i];
+		padre.appendChild(opt);
+	}
 };
+function start() {
+	var padre = document.getElementById("mySelect");
+	padre.addEventListener("change", addActivityItem, false);
+}
+
+function addActivityItem() {
+	//option is selected
+	let valor = document.getElementById("mySelect");
+	alert(valor.options[valor.selectedIndex].text);
+}
+
+window.addEventListener("load", start, false);
