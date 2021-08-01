dde7ad2b7a34b2cba620747a79818231d07b984c
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-08-01 04:49:32 +0000

    Ejercicios resueltos hasta el número 14 - Todos los ejercicios completados

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,31 @@
 // Your code here
+let campo = document.querySelector("#addToDo");
+campo.addEventListener("keypress", function(e) {
+	if (e.key === "Enter") {
+		addtask(campo.value);
+	}
+});
+//let lis = document.querySelectorAll(".fa-trash");
+/* lis.addEventListener("click", function() {
+	alert("funciona");
+}); */
+
+let listaLi = document.querySelector("ul");
+listaLi.addEventListener("click", function(e) {
+	var li = e.target.closest("li");
+	var nodes = Array.from(listaLi.children);
+	var index = nodes.indexOf(li);
+	alert(index);
+	deletetask(index);
+});
+
+function addtask(valor) {
+	let lista = document.querySelector("ul");
+	let li = document.createElement("li");
+	li.innerHTML = '<span><i class="fa fa-trash"></i></span> ' + valor;
+	lista.appendChild(li);
+}
+function deletetask(index) {
+	let lista = document.querySelector("ul");
+	lista.removeChild(lista.childNodes[index]);
+}
